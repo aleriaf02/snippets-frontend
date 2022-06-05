0499d88eb6d1ecda8cd1f708c91277fe3a30fd4d
Aitor Lería <aitorleria@gmail.com>
2022-06-05 22:36:34 +0200

    Update app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CoreModule } from './core/core.module';
-import { AuthModule } from './modules/auth/auth.module';
-import { LandingPageModule } from './modules/landing-page/landing-page.module';
-import { MainPageModule } from './modules/main-page/main-page.module';
-import { HighlightJsModule } from 'ngx-highlight-js';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { HotToastModule } from '@ngneat/hot-toast';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ServerErrorInterceptor } from './core/interceptors/server-error/server-error.interceptor';
-import { HIGHLIGHT_OPTIONS, HighlightModule } from 'ngx-highlightjs';
-import { SharedModule } from './shared/shared.module';
-import { DetailedViewModule } from './modules/detailed-view/detailed-view.module';
-import { CreationViewModule } from './modules/creation-view/creation-view.module';
-import { ProfileViewModule } from './modules/profile-view/profile-view.module';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {CoreModule} from './core/core.module';
+import {AuthModule} from './modules/auth/auth.module';
+import {LandingPageModule} from './modules/landing-page/landing-page.module';
+import {MainPageModule} from './modules/main-page/main-page.module';
+import {HighlightJsModule} from 'ngx-highlight-js';
+import {HTTP_INTERCEPTORS} from '@angular/common/http';
+import {HotToastModule} from '@ngneat/hot-toast';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {ServerErrorInterceptor} from './core/interceptors/server-error/server-error.interceptor';
+import {HIGHLIGHT_OPTIONS, HighlightModule} from 'ngx-highlightjs';
+import {SharedModule} from './shared/shared.module';
+import {DetailedViewModule} from './modules/detailed-view/detailed-view.module';
+import {CreationViewModule} from './modules/creation-view/creation-view.module';
+import {ProfileViewModule} from './modules/profile-view/profile-view.module';
+import {GoogleLoginProvider, BaseLoginProvider, SocialLoginModule} from "@abacritt/angularx-social-login";
+
+export const googleConfig = {scope: 'email', ux_mode: 'redirect', response_type: 'code'};
 
 @NgModule({
   declarations: [AppComponent],
@@ -48,7 +51,20 @@ import { ProfileViewModule } from './modules/profile-view/profile-view.module';
         themePath: 'assets/styles/solarized-dark.css',
       },
     },
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: true,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider('134169247632-318tks8l71omeg5v4vubvomn6qfkoiov.apps.googleusercontent.com', googleConfig),
+          },
+        ]
+      }
+    },
   ],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+}
